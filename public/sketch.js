1320d04b053a8107e13b4d3f24332edb27ad4488
jingxianlau <jingxian.lau16@gmail.com>
2025-07-15 10:44:31 +0800

    screenshake thingy

--- public/sketch.js
+++ public/sketch.js
@@ -2,6 +2,15 @@ let grid = [];
 let players = [];
 let p;
 let dead = false;
+let screenShake = false;
+let screenShakeCoeff = 5;
+let screenShakeTimer = 20;
+
+function shake(coeff = 5, timer = 5) {
+  screenShake = true;
+  screenShakeCoeff = coeff;
+  screenShakeTimer = timer;
+}
 
 function setup() {
   // Create canvas and attach to container
@@ -17,17 +26,39 @@ function setup() {
     p = players.find(a => a.id == socket.id);
   });
   socket.on('players', data => {
+    let len, cd, oldmana;
+    if (p) {
+      len = p.snake.segments.length;
+      cd = p.dash;
+      oldmana = p.mana;
+    }
+
     if (!dead) players = data;
+    p = players.find(a => a.id == socket.id);
+
+    if (!p) return;
+    console.log(p.snake.segments.length, len);
+    if (p.snake.segments.length > len) shake(2, 2);
+    if (p.dash == 0 && cd !== 0) shake(3, 1);
+    if (p.mana >= 20 && oldmana < 20) shake(6, 2);
   });
 
   socket.on('dead', () => {
+    shake(20, 10);
     respawn();
     socket.disconnect();
   });
 }
 
 function draw() {
-  background('black');
+  background('#333');
+  if (screenShake == true) {
+    if (--screenShakeTimer == 0) screenShake = false;
+    translate(
+      random(-screenShakeCoeff, screenShakeCoeff),
+      random(-screenShakeCoeff, screenShakeCoeff)
+    );
+  }
   if (grid.length == 0) return;
 
   for (let i = 0; i < grid.length; i++) {
@@ -66,11 +97,17 @@ function keyPressed() {
   if (['w', 'a', 's', 'd'].includes(key))
     socket.emit('action', { player: socket.id, dir: key });
 
-  if (key == ' ') {
-    socket.emit('ability', 'dash');
-  } else if (key == 'e') {
+  if (p.dash == 0) {
+    if (key == ' ') {
+      shake(3, 3);
+      socket.emit('ability', 'dash');
+    }
+  } else if (p.mana < 20) return;
+  if (key == 'e') {
+    shake(10, 10);
     socket.emit('ability', 'dash5');
   } else if (key == 'q') {
+    shake(1, 10);
     socket.emit('ability', 'uturn');
   }
 }
